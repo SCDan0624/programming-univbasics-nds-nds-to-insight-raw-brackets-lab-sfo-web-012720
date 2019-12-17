e7e57b86f9104dcdf26902f795f4d1e6556149a8
Daniel Mayle <dmayle012@gmail.com>
2019-12-17 23:09:56 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -11,7 +11,7 @@ def directors_totals(nds)
    movie_index = 0 
    
    
-   while movie_index < nds[director_index][:movies].length do 
+   while movie_index < nds[director_index][:movies].length do #Go through movies on each director hash
      total[director] += nds[director_index][:movies][movie_index][:worldwide_gross]
      movie_index += 1
    end
